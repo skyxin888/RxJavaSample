4cbf41e4654aa657da5c28e9ca6ce73615fb0e37
guxin <155173611@qq.com>
2016-09-19 16:09:34 +0800

    combining operator

--- app/src/main/java/cn/golden/rxjava/OperatorCombiningActivity.java
+++ app/src/main/java/cn/golden/rxjava/OperatorCombiningActivity.java
@@ -67,7 +67,7 @@ public class OperatorCombiningActivity extends BaseActivity{
                 mButton.setText("mergeDelayError");
                 break;
             case Opertor.Combining.zip:
-                mButton.setText("scan");
+                mButton.setText("zip");
                 break;
             case Opertor.Combining.and:
                 mButton.setText("and");
@@ -104,19 +104,203 @@ public class OperatorCombiningActivity extends BaseActivity{
             case Opertor.Combining.mergeDelayError:
                 RxforMergeDelayError();
                 break;
-            case Opertor.Transform.scan:
+            case Opertor.Combining.zip:
+                RxforZip();
                 break;
-            case Opertor.Transform.groupBy:
+            case Opertor.Combining.and:
+                RxforAnd();
                 break;
-            case Opertor.Transform.buffer:
+            case Opertor.Combining.combineLatest:
+                RxforCombineLatest();
                 break;
-            case Opertor.Transform.window:
+            case Opertor.Combining.join:
+                RxforJoin();
                 break;
-            case Opertor.Transform.cast:
+            case Opertor.Combining.switchOnNext:
+                RxforSwitch();
                 break;
             default:
                 break;
         }
+    }
+
+    private void RxforSwitch() {
+        Observable<Observable<Long>> observable = Observable.interval(0, 500, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Observable<Long>>() {
+                    @Override
+                    public Observable<Long> call(Long aLong) {
+                        return Observable.interval(0, 200, TimeUnit.MILLISECONDS).map(new Func1<Long, Long>() {
+                            @Override
+                            public Long call(Long aLong) {
+                                return aLong * 10;
+                            }
+                        }).take(5);
+                    }
+                }).take(2);
+
+        Observable.switchOnNext(observable).observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<Long>() {
+            @Override
+            public void onNext(Long item) {
+                mBuilder.append("onNext :").append(String.valueOf(item)).append("\n");
+                mResult.setText(mBuilder);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                mBuilder.append("onError \n");
+                mResult.setText(mBuilder.toString());
+            }
+
+            @Override
+            public void onCompleted() {
+                mBuilder.append("onCompleted \n");
+                mResult.setText(mBuilder.toString());
+                mBuilder.setLength(0);
+            }
+        });
+    }
+
+    private void RxforJoin() {
+        //产生0,3,6,9,12数列
+        Observable<Long> observable1 = Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong * 3;
+                    }
+                }).take(5);
+
+        //产生0,10,20,30,40数列
+        Observable<Long> observable2 = Observable.interval(500, 1000, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong * 10;
+                    }
+                }).take(5);
+
+        observable1.join(observable2, new Func1<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Long aLong) {
+                //使Observable延迟600毫秒执行
+                return Observable.just(aLong).delay(600, TimeUnit.MILLISECONDS);
+            }
+        }, new Func1<Long, Observable<Long>>() {
+            @Override
+            public Observable<Long> call(Long aLong) {
+                //使Observable延迟600毫秒执行
+                return Observable.just(aLong).delay(600, TimeUnit.MILLISECONDS);
+            }
+        }, new Func2<Long, Long, Long>() {
+            @Override
+            public Long call(Long aLong, Long aLong2) {
+                return aLong + aLong2;
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Long>() {
+            @Override
+            public void onNext(Long item) {
+                mBuilder.append("onNext :").append(String.valueOf(item)).append("\n");
+                mResult.setText(mBuilder);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                mBuilder.append("onError \n");
+                mResult.setText(mBuilder.toString());
+            }
+
+            @Override
+            public void onCompleted() {
+                mBuilder.append("onCompleted \n");
+                mResult.setText(mBuilder.toString());
+                mBuilder.setLength(0);
+            }
+        });
+    }
+
+    private void RxforCombineLatest() {
+        //产生0,3,6,9数列
+        Observable<Long> observable1 = Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong * 3;
+                    }
+                }).take(4);
+
+        //产生0,10,20,30,40数列
+        Observable<Long> observable2 = Observable.interval(500, 1000, TimeUnit.MILLISECONDS)
+                .map(new Func1<Long, Long>() {
+                    @Override
+                    public Long call(Long aLong) {
+                        return aLong * 10;
+                    }
+                }).take(5);
+
+
+        Observable.combineLatest(observable1, observable2, new Func2<Long, Long, Long>() {
+            @Override
+            public Long call(Long aLong, Long aLong2) {
+                return aLong+aLong2;
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Long>() {
+            @Override
+            public void onNext(Long item) {
+                mBuilder.append("onNext :").append(String.valueOf(item)).append("\n");
+                mResult.setText(mBuilder);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                mBuilder.append("onError \n");
+                mResult.setText(mBuilder.toString());
+            }
+
+            @Override
+            public void onCompleted() {
+                mBuilder.append("onCompleted \n");
+                mResult.setText(mBuilder.toString());
+                mBuilder.setLength(0);
+            }
+        });
+    }
+
+    private void RxforAnd() {
+
+    }
+
+    private void RxforZip() {
+        Observable<Integer> observable1 = Observable.just(10,20,30,40);
+        Observable<Integer> observable2 = Observable.just(4, 8, 12);
+        Observable.zip(observable1, observable2, new Func2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        }).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onNext(Integer item) {
+                mBuilder.append("onNext :").append(String.valueOf(item)).append("\n");
+                mResult.setText(mBuilder);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                mBuilder.append("onError :").append(error.getMessage()).append("\n");
+                mResult.setText(mBuilder.toString());
+            }
+
+            @Override
+            public void onCompleted() {
+                mBuilder.append("onCompleted \n");
+                mResult.setText(mBuilder.toString());
+                mBuilder.setLength(0);
+            }
+        });
+
     }
 
     private void RxforMergeDelayError() {
@@ -167,7 +351,7 @@ public class OperatorCombiningActivity extends BaseActivity{
 
     private void RxforMerge() {
         //产生0,3,6,9,12数列
-        Observable<Long> odds = Observable.timer(0,1000, TimeUnit.MILLISECONDS)
+        Observable<Long> odds = Observable.interval(0,1000, TimeUnit.MILLISECONDS)
                 .map(new Func1<Long, Long>() {
                     @Override
                     public Long call(Long aLong) {
@@ -176,7 +360,7 @@ public class OperatorCombiningActivity extends BaseActivity{
                 }).take(5);
 
         //产生0,10,20,30,40数列
-        Observable<Long> evens = Observable.timer(500,1000,TimeUnit.MILLISECONDS)
+        Observable<Long> evens = Observable.interval(500,1000,TimeUnit.MILLISECONDS)
                 .map(new Func1<Long, Long>() {
                     @Override
                     public Long call(Long aLong) {
